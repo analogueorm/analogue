daeb06d7f0eb6ad6138fde20afdbe753d9e9afaf
Rémi Collin <remi@plateau.io>
2015-03-17 14:17:29 +0100

    Add Require-dev

--- src/System/ScopeInterface.php
+++ src/System/ScopeInterface.php
@@ -3,7 +3,7 @@
 interface ScopeInterface {
 
 	/**
-	 * Apply the scope to a given Eloquent query builder.
+	 * Apply the scope to a given Query builder.
 	 *
 	 * @param  \Analogue\ORM\System\Query  $builder
 	 * @return void
@@ -11,7 +11,7 @@ interface ScopeInterface {
 	public function apply(Query $builder);
 
 	/**
-	 * Remove the scope from the given Eloquent query builder.
+	 * Remove the scope from the Query builder.
 	 *
 	 * @param  \Analogue\ORM\System\Query  $builder
 	 * @return void
