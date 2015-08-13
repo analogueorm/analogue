19ac2bf4fc2ffc5fec84c3b81ce4446a39f9e72c
Mark Wales <mark@smallhadroncollider.com>
2015-08-13 11:58:05 +0100

    fixed issues with Illuminate 5.1 'list' method

--- src/EntityCollection.php
+++ src/EntityCollection.php
@@ -5,6 +5,7 @@ use InvalidArgumentException;
 use Analogue\ORM\Mappable;
 use Analogue\ORM\System\Manager;
 use Illuminate\Support\Collection as Collection;
+use Illuminate\Support\Arr;
 
 class EntityCollection extends Collection {
 	
@@ -80,6 +81,18 @@ class EntityCollection extends Collection {
 
 		return $this->pull($entity->getEntityAttribute($keyName));
 	}
+
+    /**
+     * Get an array with the values of a given key.
+     *
+     * @param  string  $value
+     * @param  string  $key
+     * @return static
+     */
+    public function pluck($value, $key = null)
+    {
+        return new Collection(Arr::pluck($this->items, $value, $key));
+    }
 
 	/**
 	 * Push an item onto the beginning of the collection.
