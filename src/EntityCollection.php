2ecb7dc3e222c890d7013cc6fb2125ead593706a
Travis Bennett <tandrewbennett@hotmail.com>
2016-08-26 22:10:34 +0000

    Fix a small compatability bug with 5.3.
    - This commit contains a small bugfix for compatability with the
    Illuminate\Support 5.3 package. Namely, the unique method now takes an
    optional strict parameter.

--- src/EntityCollection.php
+++ src/EntityCollection.php
@@ -43,7 +43,7 @@ class EntityCollection extends Collection
         }
 
         return array_first($this->items, function ($entity, $itemKey) use ($key) {
-            
+
             return $this->getEntityKey($entity) == $key;
         }, $default);
     }
@@ -383,10 +383,11 @@ class EntityCollection extends Collection
      * Return only unique items from the collection.
      *
      * @param  string|null $key
+     * @param  bool $strict
      * @throws MappingException
      * @return self
      */
-    public function unique($key = null)
+    public function unique($key = null, $strict = false)
     {
         $dictionary = $this->getDictionary();
 
