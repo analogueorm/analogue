6330dac0d42f1d2f7226b1873ac66c1ffec6070f
Rémi Collin <remi@plateau.io>
2016-10-24 12:39:22 +0200

    Add tests for previous commit(

--- tests/cases/EntityCollectionTest.php
+++ tests/cases/EntityCollectionTest.php
@@ -11,16 +11,26 @@ class EntityCollectionTest extends DomainTestCase
     }
 
     /** @test */
-    public function we_can_convert_a_collection_to_json()
+    public function we_can_convert_a_collection_to_array()
     {
-        for($x=0; $x<=5;$x++) {
+        for($x=0; $x<5;$x++) {
             $blog = $this->factoryCreateUid(Blog::class);
         }
         $mapper = $this->mapper(Blog::class);
         $blogs = $mapper->all();
-
-        dd($blogs->toJson());
-
+        $this->assertCount(5, $blogs->toArray());
     }
 
+    /** @test */
+    public function we_can_convert_a_collection_to_json()
+    {
+        for($x=0; $x<5;$x++) {
+            $blog = $this->factoryCreateUid(Blog::class);
+        }
+        $mapper = $this->mapper(Blog::class);
+        $blogs = $mapper->all();
+        $json = $blogs->toJson();
+        $this->assertCount(5, json_decode($json));
+    }
+    
 }
