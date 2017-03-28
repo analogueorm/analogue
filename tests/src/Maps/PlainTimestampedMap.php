6e80c618fdbb1c8424fea8ae517cadda47ccb6b6
Remi Collin <rcollin2@gmail.com>
2017-03-28 08:47:02 +0200

    Fix stub

--- tests/src/Maps/PlainTimestampedMap.php
+++ tests/src/Maps/PlainTimestampedMap.php
@@ -6,15 +6,19 @@ use Analogue\ORM\EntityMap;
 
 class PlainTimestampedMap extends EntityMap
 {
+	protected $table = "timestampeds";
+
+	protected $arrayName = null;
+
 	protected $properties = [
 		'id' => 'id',
-		'created_at' => 'createdAt',
-		'updated_at' => 'updatedAt',
+		'created_at' => 'created_at',
+		'updated_at' => 'updated_at',
 	];
 
-	protected $createdAtColumn = 'createdAt';
+	protected $createdAtColumn = 'created_at';
 
-	protected $updatedAtColumn = 'updatedAt';
+	protected $updatedAtColumn = 'updated_at';
 
     public $timestamps = true;
 
--- tests/src/PlainTimestamped.php
+++ tests/src/PlainTimestamped.php
@@ -4,7 +4,6 @@ namespace TestApp;
 
 class PlainTimestamped {
 
-	protected $table = "timestampeds";
 
 	protected $id;
 
