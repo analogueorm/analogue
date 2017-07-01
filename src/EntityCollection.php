1a8c6f61996756df6df0dbf03424a2d38d886b5d
RemiCollin <rcollin2@gmail.com>
2017-07-01 11:00:58 +0000

    Apply fixes from StyleCI

--- src/EntityCollection.php
+++ src/EntityCollection.php
@@ -426,13 +426,15 @@ class EntityCollection extends Collection
     /**
      * Unset the item at a given offset.
      *
-     * @param  string  $key
+     * @param string $key
+     *
      * @return void
      */
     public function offsetUnset($key)
     {
-        $this->items = array_filter($this->items, function($item) use ($key) {
+        $this->items = array_filter($this->items, function ($item) use ($key) {
             $primaryKey = $this->getEntityKey($item);
+
             return $primaryKey !== $key;
         });
     }
