cbf40c53b123163ae14fac7fc6ce7dda6daa6da7
RemiCollin <rcollin2@gmail.com>
2017-12-06 08:44:41 +0000

    Apply fixes from StyleCI

--- src/Notifications/NotificationMap.php
+++ src/Notifications/NotificationMap.php
@@ -6,32 +6,30 @@ use Analogue\ORM\EntityMap;
 
 class NotificationMap extends EntityMap
 {
-	/**
-      * Get the entity's notifications.
-      */
-     public function notifications()
-     {
-         return $this->morphMany(DatabaseNotification::class, 'notifiable')
-                     ->orderBy('created_at', 'desc');
-     }
- 
-     /**
-      * Get the entity's read notifications.
-      */
-     public function readNotifications()
-     {
-         return $this->notifications()
-                     ->whereNotNull('read_at');
-     }
- 
-     /**
-      * Get the entity's unread notifications.
-      */
-    public function unreadNotifications()     
+    /**
+     * Get the entity's notifications.
+     */
+    public function notifications()
     {
-         return $this->notifications()
-                     ->whereNull('read_at');
+        return $this->morphMany(DatabaseNotification::class, 'notifiable')
+                     ->orderBy('created_at', 'desc');
     }
 
-}
+    /**
+     * Get the entity's read notifications.
+     */
+    public function readNotifications()
+    {
+        return $this->notifications()
+                     ->whereNotNull('read_at');
+    }
 
+    /**
+     * Get the entity's unread notifications.
+     */
+    public function unreadNotifications()
+    {
+        return $this->notifications()
+                     ->whereNull('read_at');
+    }
+}
