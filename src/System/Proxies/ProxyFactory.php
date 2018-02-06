3c92bc79bb9b0a739855c4be26cf873505956341
RemiCollin <rcollin2@gmail.com>
2018-02-06 11:46:00 +0000

    Apply fixes from StyleCI

--- src/System/Proxies/ProxyFactory.php
+++ src/System/Proxies/ProxyFactory.php
@@ -48,7 +48,6 @@ class ProxyFactory
         $proxyPath = Manager::getInstance()->getProxyPath();
 
         if ($proxyPath !== null) {
-
             $proxyConfig = new Configuration();
             $proxyConfig->setProxiesTargetDir($proxyPath);
 
